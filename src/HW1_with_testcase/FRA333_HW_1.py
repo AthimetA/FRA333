a23c20ff41f36db50a63bfecc659906ffb1464ca
AthimetU <mobiltiy2604@gmail.com>
2022-09-14 20:14:51 +0700

    basic transformation

--- src/HW1_with_testcase/FRA333_HW_1.py
+++ src/HW1_with_testcase/FRA333_HW_1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from this import d, s
 from trackbeebot import BeeBot
 import matplotlib.pyplot as plt 
 from matplotlib.patches import Polygon
@@ -9,10 +10,14 @@ import json
 class MyBeeBot(BeeBot):
     def __init__(self,a_i):
         super().__init__(a_i)
-    # a_i is initial position
-    # c is command {'0'->stop, '1'->forward, '2'->backward, '3'->turn right, '4'->turn left}
-    # o is obstacle
+        # a_i is initial position
+        # c is command {'0'->stop, '1'->forward, '2'->backward, '3'->turn right, '4'->turn left}
+        # o is obstacle
         self.curPos = np.vstack((np.array(a_i).transpose(),[1]))
+        # Defult transformation matrix is y = y+1
+        self.tranformationMatrix = np.array([[1, 0, 0],
+                                             [0, 1, 1],
+                                             [0, 0, 1]])
     
     def trackBeeBot(self, com, W):
     # a_i is initial position
@@ -35,8 +40,12 @@ class MyBeeBot(BeeBot):
             if self.checkCollision(W):
                 print('Collision')
                 break
+                
+        return self.curPos
       
-
+    def doTranfromation(self):
+        self.curPos = np.matmul(self.tranformationMatrix,self.curPos)
+    
     def checkCollision(self, W):
         if self.curPos[0] > W[0] or self.curPos[0] < 0 or self.curPos[1] > W[1] or self.curPos[1] < 0:
             return True
@@ -60,14 +69,8 @@ class MyBeeBot(BeeBot):
         Rotate_Z_matrix = np.array([[zC, -zS, 0],
                                     [zS, zC, 0],
                                     [0, 0, 1]])
-        Translate_matrix_minus = np.array([[1, 0, -dX],
-                                            [0, 1, -dY],
-                                            [0, 0, 1]])
-        # print(np.array([[zC, -zS, dX],
-        #                 [zS, zC, dY],
-        #                 [0, 0, 1]]))
-        print(np.dot(Translate_matrix,np.dot(Rotate_Z_matrix,Translate_matrix_minus)))
-        return np.dot(Translate_matrix_minus,np.dot(Rotate_Z_matrix,Translate_matrix))
+        Tranformation_matrix = np.dot(Rotate_Z_matrix,Translate_matrix)
+        return np.dot(Tranformation_matrix,self.tranformationMatrix)
     
     def matrix(self,rotation, translation):
         xC, xS = self.sinCos(rotation[0])
@@ -95,7 +98,6 @@ class MyBeeBot(BeeBot):
         return np.dot(Rotate_Z_matrix,np.dot(Rotate_Y_matrix,np.dot(Rotate_X_matrix,Translate_matrix)))
     
     def test(self):
-        tr = self.hTranformation2D([90],[1,1])
         # dX = 1
         # dY = 2
         # zC, zS = self.sinCos(-90)
@@ -113,9 +115,13 @@ class MyBeeBot(BeeBot):
         # print('-------5---------')
         # RT = np.matmul(R,T)
         # print(f'{RP} \n   +  \n{RT}  \n = \n{np.sum([RP,RT],axis=0)}')
-        print('-------6---------')
-        self.curPos = np.matmul(tr,self.curPos)
-        print(self.curPos)
+        print('-------**---------')
+        self.tranformationMatrix = self.hTranformation2D([-90],[3,0])
+        print(f'self.tranformationMatrix =\n  {self.tranformationMatrix}\n')
+        print(f'self.curPos =\n  {self.curPos}\n')
+        self.doTranfromation()
+        print(f'self.curPos(New) =\n  {self.curPos}\n')
+        print('-------**---------')
     
 
 mytest = MyBeeBot(np.array([[0,0]]))
