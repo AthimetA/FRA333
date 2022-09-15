c5313afe512c24769ce51c24d5eac51c5a221b3f
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2022-09-15 21:40:53 +0700

    before edit

--- src/HW1_with_testcase/FRA333_HW_1.py
+++ src/HW1_with_testcase/FRA333_HW_1.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from this import d, s
 from trackbeebot import BeeBot
 import matplotlib.pyplot as plt 
 from matplotlib.patches import Polygon
@@ -15,8 +14,17 @@ class MyBeeBot(BeeBot):
         # o is obstacle
         self.posHex = np.vstack((np.array(a_i).transpose(),[1]))
         self.posCar = np.vstack((self.idx2pos(self.posHex[0],self.posHex[1]).reshape(2,1),[1]))
+        # Our bee bot is a hexagonal grid
+        # self.linearTrans = np.array([[1/3,np.sqrt(3)/3,0],[-1/3, np.sqrt(3)/3,0],[0,0,1]])
+        # self.ltransCheck = np.matmul(self.linearTrans,self.posCar)
+        # self.det = np.linalg.det(self.linearTrans)
+        # self.rotTrans = np.array([[np.cos(np.pi/3), -np.sin(np.pi/3)],[np.sin(np.pi/3), np.cos(np.pi/3)]])
         # Defult transformation matrix is hexagonal pos (+1,+1)
-        self.tranformationMatrix = self.idx2pos(self.hTranformation2D([60],[1,1])[0][2],self.hTranformation2D([60],[1,1])[1][2])
+        self.tranformationMatrix = np.array([[1, 0, 0],
+                                            [0, 1, np.sqrt(3)],
+                                            [0, 0, 1]])
+        
+        self.check = np.matmul(self.hTranformation2D([-60],[0,0]),self.posHex)
     
     def trackBeeBot(self, com, W):
     # a_i is initial position
@@ -49,10 +57,11 @@ class MyBeeBot(BeeBot):
             return False
     
     def pos2idx(self, posCartesian):
-        x = posCartesian[0]
-        y = posCartesian[1]
-        poshexagonal = np.array([(x+np.sqrt(3)*y)/3,(-x+np.sqrt(3)*y)/3,posCartesian[2]])
-        return poshexagonal
+        self.linearTrans = np.array([[1/3,np.sqrt(3)/3,0],[-1/3, np.sqrt(3)/3,0],[0,0,1]])
+        # x = posCartesian[0]
+        # y = posCartesian[1]
+        # poshexagonal = np.array([(x+np.sqrt(3)*y)/3,(-x+np.sqrt(3)*y)/3,posCartesian[2]])
+        return np.matmul(self.linearTrans,posCartesian)
     
     def sinCos(self,angle):
         r = np.radians(angle)
@@ -92,80 +101,41 @@ class MyBeeBot(BeeBot):
     def test(self):
         print('-------*1*---------')
         print(f'self.curPos(old) =\n  {self.posCar}\n')
-        self.posCar = np.matmul(self.hTranformation2D([0],[0,1]),self.posCar)
+        self.posCar = np.matmul(self.tranformationMatrix,self.posCar)
+        print(f'self.tranformationMatrix =\n  {self.tranformationMatrix}\n')
         print(f'self.curPos(New) =\n  {self.posCar}\n')
+        print(f'self.hexPos(New) =\n  {self.pos2idx(self.posCar)}\n')
         print('-------*1*---------')
+    
+        print('-------*2*---------')
+        # print(f'self.curPos(old) =\n  {self.posCar}\n')
+        zC, zS = self.sinCos(60)
+        Rotate_Z_matrix = np.array([[zC, -zS, 0],
+                                    [zS, zC, 0],
+                                    [0, 0, 1]])
+        # self.tranformationMatrix = np.matmul(self.hTranformation2D([60],[0,0]),self.tranformationMatrix)
+        print(f'self.tranformationMatrix =\n  {self.tranformationMatrix}\n')
+        self.tranformationMatrix = np.matmul(Rotate_Z_matrix,self.tranformationMatrix)
+        print(f'self.tranformationMatrix =\n  {self.tranformationMatrix}\n')
+        self.posCar = np.matmul(self.tranformationMatrix,self.posCar)
+        print(f'self.curPos(New) =\n  {self.posCar}\n')
+        # print(f'self.hexPos(New) =\n  {self.pos2idx(self.posCar)}\n')
+        print('-------*2*---------')
         
-        # print('-------*2*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # # self.curPos = np.matmul(self.Tranformation2D([120],[0,0]),self.curPos)
-        # # self.curPos = np.matmul(self.Tranformation2D([60],[0,0]),self.curPos)
-        # self.curPos = np.matmul(self.Tranformation2D([60+30],[-1,-1]),self.curPos)
-        # # self.curPos = np.matmul(self.hTranformation2D([60+60],[-1,-1]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*2*---------')
-        
-        # print('-------*3*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # self.curPos = np.matmul(self.hTranformation2D([0],[1,1]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*3*---------')
-        
-        # print('-------*4*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # self.curPos = np.matmul(self.hTranformation2D([-180],[0,0]),self.curPos)
-        # self.curPos = np.matmul(self.hTranformation2D([90],[0,0]),self.curPos)
-        # self.curPos = np.matmul(self.hTranformation2D([0],[1,1]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*4*---------')
-        
-        # print('-------*2*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # self.curPos = np.matmul(self.hTranformation2D([0],[-1,-1]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*2*---------')
-        
-        # print('-------*0*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # self.curPos = np.matmul(self.hTranformation2D([-60],[0,0]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*0*---------')
-        
-        # print('-------*1*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # self.curPos = np.matmul(self.hTranformation2D([-90],[0,0]),self.curPos)
-        # self.curPos = np.matmul(self.hTranformation2D([-90],[0,0]),self.curPos)
-        # self.curPos = np.matmul(self.hTranformation2D([-90],[0,0]),self.curPos)
-        # self.curPos = np.matmul(self.hTranformation2D([0],[0,1]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*1*---------')
-        
-        # print('-------*2*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # self.curPos = np.matmul(self.hTranformation2D([-90],[0,0]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*2*---------')
-        
-        # print('-------*3*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # self.curPos = np.matmul(self.hTranformation2D([0],[0,1]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*3*---------')
-        
-        # print('-------*4*---------')
-        # print(f'self.curPos(old) =\n  {self.curPos}\n')
-        # self.curPos = np.matmul(self.hTranformation2D([45],[0,0]),self.curPos)
-        # print(f'self.curPos(New) =\n  {self.curPos}\n')
-        # print('-------*4*---------')
 
 # mytest = MyBeeBot(np.array([[7.5,-4.330127018922193]]))
 mytest = MyBeeBot(np.array([[0,-5]]))
-W = np.array([[5, 2], [5, 4]])
+# mytest = MyBeeBot(np.array([[1,-4]]))
+# W = np.array([[5, 2], [5, 4]])
 # mytest.test()
 print(mytest.posHex)
 print('-------*1*---------')
 print(mytest.pos2idx(mytest.posCar))
 print('-------*2*---------')
+print(mytest.posCar)
+print('-------*3*---------')
 print(mytest.tranformationMatrix)
+print('-------*4*---------')
+mytest.test()
 
 # print(mytest.idx2pos(0,-5))
