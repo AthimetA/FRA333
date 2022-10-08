c64684b41e17f3a958b900a546e314a95183c7c0
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2022-10-08 18:10:52 +0700

    clear old ver

--- src/fra333_lab2_25_v1/Note/rotateZ.m
+++
@@ -1,3 +0,0 @@
-function matrix = rotateZ(theta)
-    matrix = [cos(theta) -sin(theta) 0 0;sin(theta)  cos(theta) 0 0;0 0 1 0; 0 0 0 1];
-end
--- src/fra333_lab2_25_v1/Note/transZ.m
+++
@@ -1,3 +0,0 @@
-function matrix = transZ(z)
-    matrix = [1 0 0 0 ; 0 1 0 0; 0 0 1 z; 0 0 0 1];
-end
