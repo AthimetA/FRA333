1b6d3209a2154334897366692655f89e48afecb0
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2022-10-31 21:34:56 +0700

    Add comment on HW2

--- src/HW2_6362_6368/fra333_hw2.py
+++ src/HW2_6362_6368/fra333_hw2.py
@@ -16,7 +16,6 @@ def endEffectorJacobianHW2(q):
     '''
         q : format list 1x3 [[i_11, i_12, i_13]]
         q unit: rad
-        type something here
     '''
     # R = Rotation matrix of each joint[i] via GlobalFrame [3x3x4]
     # P = Position of Frame[i] via GlobalFrame [3x4]
@@ -39,7 +38,7 @@ def endEffectorJacobianHW2(q):
         [132. 232. 332. 432.]
         [133. 233. 333. 433.]]
         ]
-        
+    
     R0_1 = 
         [[111. 112. 113.]
         [121. 122. 123.]
@@ -48,27 +47,30 @@ def endEffectorJacobianHW2(q):
     # Since all the joints are revolute, the Jacobian p = 1
     rho = [1,1,1]
     
-    R0_1 = R[:,:,0]
-    R0_2 = R[:,:,1]
-    R0_3 = R[:,:,2]
-    R0_4 = R[:,:,3] # R0_4 = R_e
+    # Pick the rotation matrix of each joint (Frame[i]) via GlobalFrame
+    R0_1 = R[:,:,0] # Frame[1] via GlobalFrame
+    R0_2 = R[:,:,1] # Frame[2] via GlobalFrame
+    R0_3 = R[:,:,2] # Frame[3] via GlobalFrame
     
     # Find the Jacobian matrix of angular velocity for End Effector
     Z = np.array([[0],[0],[1]]) # Z axis of Local Frame [3x1]
-    JW0_1 = rho[0] * (R0_1 @ Z)
-    JW0_2 = rho[0] * (R0_2 @ Z)
-    JW0_3 = rho[0] * (R0_3 @ Z)
+    # Calculate the angular velocity Jacobian of each joint
+    JW0_1 = rho[0] * (R0_1 @ Z) 
+    JW0_2 = rho[1] * (R0_2 @ Z)
+    JW0_3 = rho[2] * (R0_3 @ Z)
+    # Stack the angular velocity Jacobian of each joint to form the Jacobian matrix of angular velocity for End Effector [3x3]
     JW = np.concatenate((JW0_1,JW0_2,JW0_3),axis=1)
     
     # Find the Jacobian matrix of linear velocity for End Effector
-    P0_01 = P[:,0]
-    P0_02 = P[:,1]
-    P0_03 = P[:,2]
-    P0_04 = P[:,3] # P0_04 = p_e
-    P0_E = p_e
+    P0_01 = P[:,0] # Position of Frame[1] via GlobalFrame [3x1]
+    P0_02 = P[:,1] # Position of Frame[2] via GlobalFrame [3x1]
+    P0_03 = P[:,2] # Position of Frame[3] via GlobalFrame [3x1]
+    P0_E = p_e # Position of End Effector via GlobalFrame [3x1]
+    # Calculate the linear velocity Jacobian of each joint
     JV0_1 = ((1-rho[0]) * (R0_1 @ Z)) + np.cross(JW0_1.reshape(1,3),(P0_E-P0_01)).reshape(3,1)
     JV0_2 = ((1-rho[1]) * (R0_1 @ Z)) + np.cross(JW0_2.reshape(1,3),(P0_E-P0_02)).reshape(3,1)
     JV0_3 = ((1-rho[2]) * (R0_1 @ Z)) + np.cross(JW0_3.reshape(1,3),(P0_E-P0_03)).reshape(3,1)
+    # Stack the linear velocity Jacobian of each joint to form the Jacobian matrix of linear velocity for End Effector [3x3]
     JV = np.concatenate((JV0_1,JV0_2,JV0_3),axis=1)
         
     '''
@@ -80,6 +82,7 @@ def endEffectorJacobianHW2(q):
           [i_51, i_52, i_53],
           [i_61, i_62, i_63] ]
     '''
+    # Stack the Jacobian matrix of angular velocity and linear velocity to form the Jacobian matrix of End Effector [6x3]
     J_e = np.concatenate((JW,JV),axis=0)
     
     return J_e
@@ -90,16 +93,22 @@ def checkSingularityHW2(q):
     '''
         q : format list 1x3 [[i_11, i_12, i_13]]
         q unit: rad
-        type something here
     '''
+    # J_e = Jacobian matrix of End Effector [6x3]
+    # J_e_star = Reduced Jacobian matrix of End Effector [3x3]
     J_e = endEffectorJacobianHW2(q)
+    # We only need the last 3 rows of the Jacobian matrix of End Effector it represents the Task Space linear velocity in the Global Frame [X,Y,Z]
     J_e_star = J_e[3:6,:]
+    # Find the determinant of Reduced Jacobian matrix of End Effector to check singularity
+    # If value of the determinant is undefined(subtrack by zero) the return value is 0.0 So, it always below the threshold
     J_e_star_det = np.linalg.det(J_e_star)
     
     '''
         return format bool
     '''
+    # Set threshold of Singularity = 0.001 
     threshold = 0.001
+    # If the absolute value of determinant of Reduced Jacobian matrix is 0 or below threshold, then the robot is in singularity
     if np.abs(J_e_star_det) < threshold:
         return True
     else:
@@ -110,25 +119,27 @@ def computeEffortHW2(q,w):
     '''
         q : format list 1x3 [[i_11, i_12, i_13]]
         q unit: rad
-        type something here
     '''
+    # Get the Rotation matrix of each Frame[i] via GlobalFrame and Position of Frame[i] via GlobalFrame and End Effector via GlobalFrame
     R,P,R_e,p_e = FKHW2(q)
+    # Get the Jacobian matrix of End Effector
     J_e = endEffectorJacobianHW2(q)
     
+    # Get the Moement and Force of End Effector
+    w_t = np.transpose(w) # Since w is a 1x6 vector, we need to transpose it to 6x1
+    N = w_t[0:3] # Moment of End Effector [3x1]
+    F = w_t[3:6] # Force of End Effector [3x1]
+    
+    # Find the effort of each joint
+    # Since Sensor is attached to End Effector, the given Moment and Force of End Effector need to be transformed to GlobalFrame
+    # Get Rotation matrix that transforms the values of w from End Effector Frame to Global Frame
     R0_e = R_e # R0_4 = R_e
-    
-    w_t = np.transpose(w)
-    N = w_t[0:3]
-    F = w_t[3:6]
-    
-    W0_e = np.concatenate((R0_e @ N,R0_e @ F),axis=0)
-    J_e_transpose = np.transpose(J_e)
-    
-    effort = J_e_transpose @ W0_e
-    
+    W0_e = np.concatenate((R0_e @ N,R0_e @ F),axis=0) # Stack Moment and Force of End Effector to form the vector of w in GlobalFrame [6x1]
+    # Calculate the effort of each joint
+    J_e_transpose = np.transpose(J_e) # Since J_e is a 6x3 matrix, we need to transpose it to 3x6 so that we can multiply it with W0_e [6x1]
+    effort = J_e_transpose @ W0_e # [3x6] @ [6x1] = [3x1]
     '''
         return format list 1x3
         [ [i_11, i_12, i_13] ]
     '''
-
-    return np.transpose(effort)
+    return np.transpose(effort) # Since effort is a 3x1 vector, we need to transpose it to 1x3
