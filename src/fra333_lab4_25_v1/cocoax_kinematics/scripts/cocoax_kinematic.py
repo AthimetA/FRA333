0689ea402069fa8e5e11c7a9bfbe0309fa0d8c04
AthimetU <mobiltiy2604@gmail.com>
2022-12-09 22:42:11 +0700

    before clear test code

--- src/fra333_lab4_25_v1/cocoax_kinematics/scripts/cocoax_kinematic.py
+++ src/fra333_lab4_25_v1/cocoax_kinematics/scripts/cocoax_kinematic.py
@@ -33,41 +33,111 @@ class CocoaVKinematic(Node):
         # Cocoa link length in meter
         self.link_length = [0.1360,0.26179,0.200]
         self.home_config = [0.0,0.0,0.0] # [q1,q2,q3]
-        self.test_config = [3.14,0,3.14] # [q1,q2,q3]
 
         # Create a timer to update the robot state
         self.timer = self.create_timer(1/self.rate, self.timer_callback)
         
-        # Forward Kinematic
-        if self.knimatic_type == 'forward':
-            # NOT DONEEEEEEEEEEEEEEEEEEEEE
-            # Subscribers
-            self.joint_state_buffer = JointState()
-            self.joint_state_subscriber = self.create_subscription(JointState, '/joint_states', self.joint_state_callback, qos_profile)
-            
-            # Publishers
-            self.forward_kinematic_buffer = JointState()
-            self.forward_kinematic_pub = self.create_publisher(JointState, '/cocoax_end_effector_states', qos_profile)
-        # Inverse Kinematic
-        elif self.knimatic_type == 'inverse':
-            # NOT DONEEEEEEEEEEEEEEEEEEEEE
-            # Subscribers
-            self.end_effector_state_buffer = JointState()
-            self.end_effector_state_subscriber = self.create_subscription(JointState, '/cocoax_end_effector_states', self.joint_state_callback, qos_profile)
-            
-            # Publishers
-            self.joint_state_buffer = JointState()
-            self.joint_state_pub = self.create_publisher(JointState, '/joint_states', qos_profile)
+        # Test procedure
+        #############################################################################
+        self.test_config = [1.0,0.1,1.0] # [q1,q2,q3]
+        R, P, R0_e, P0_e = self.cocoax_forward_position_kinematic(self.test_config) 
+        self.test_pos = P0_e
+        self.test_vel = [0.1,0.2,0.1]
         
-    def timer_callback(self):
-        pass
-    
-    
+        # Test IVK
+        q, qdot = self.cocoax_inverse_velocity_kinematic(X=self.test_pos,X_dot=self.test_vel)
+        print('q: \n', q)
+        print('qdot: \n', qdot)
+        
+        # Test FVK
+        X_dot = self.cocoax_forward_velocity_kinematic(q=q,q_dot=qdot)
+        print('X_dot: \n', X_dot)
+        #############################################################################
+        
+        
     def joint_state_callback(self, msg: JointState):
         self.joint_state_buffer = msg
         # self.get_logger().info('Joint State Received')
         # self.get_logger().info(str(self.joint_state_buffer))
         # self.forward_kinematic()
+    
+    def timer_callback(self):
+        pass
+
+    def cocoax_inverse_velocity_kinematic(self, X=[0.0,0.0,0.0], X_dot=[0.0,0.0,0.0]):
+        # Get Joint position configuration
+        q, s = self.cocoax_inverse_position_kinematic(X)
+        R, P, R0_e, P0_e = self.cocoax_forward_position_kinematic(q) 
+        print('*'*50)
+        print('Check Position')
+        print('X   : ', X    , '  q:', str(self.test_config))
+        print('P0_e: ', P0_e , '  q:', str(q))
+        print('*'*50)
+        '''
+            X_dot =[[Vx],       q_dot = [[q1_dot],
+                    [Vy],                [q2_dot],
+                    [Vz]]                [q3_dot]]
+                    
+        Inverse Velocity Kinematic can be calculated by using Jacobian Inverse
+                    q_dot = J_v_inv * X_dot
+        '''
+        # Get Jacobian Matrix
+        J_e = self.cocoax_jacobian_matrix(q)
+        print('Jacobian Matrix(linear velocity only)')
+        J_v = J_e[3:6,:]
+        print('J_v: \n', J_v)
+        print('*'*50)
+        # Check the Jacobian singularity
+        threshold = 0.001
+        if np.abs(np.linalg.det(J_v)) < threshold:
+            print('Jacobian is singular')
+            print(np.abs(np.linalg.det(J_v)))
+            self.get_logger().info('Jacobian is singular')
+            return None
+        # Get Matrix
+        qMatrix = np.matrix([q]).T
+        X_dotMatrix = np.matrix([X_dot]).T
+        J_vMatrix = np.matrix(J_v)
+        # Calculate Inverse Jacobian
+        J_v_inv = np.linalg.inv(J_vMatrix)
+        # Calculate Joint Velocity
+        q_dot = np.matmul(J_v_inv,X_dotMatrix)
+        q_dot = np.array(q_dot.T)[0]
+        # Return joint velocity as [q1_dot,q2_dot,q3_dot]
+        return q , q_dot
+    
+    def cocoax_forward_velocity_kinematic(self, q=[0.0,0.0,0.0], q_dot=[0.0,0.0,0.0]):
+        '''
+           X = [[row],             [[x],
+                [pitch],            [y],
+                [yaw],     to       [z]]
+                [x],
+                [y],
+                [z]]
+
+            J_e = [ [Jw_x],        [[Jv_x],
+                    [Jw_y],  to     [Jv_y],
+                    [Jw_z],         [Jv_z]]
+                    [Jv_x],
+                    [Jv_y],
+                    [Jv_z] ] 
+            
+        CocoaX robot is a 3-DOF robot, we decide to task space variable 
+            - position [x,y,z]
+            - velocity [vx,vy,vz]
+        '''
+        J_e = self.cocoax_jacobian_matrix(q)
+        J_v = J_e[3:6,:]
+        # Get Matrix
+        qMatrix = np.matrix([q]).T
+        q_dotMatrix = np.matrix([q_dot]).T
+        J_vMatrix = np.matrix(J_v)
+        # Calculate Twist
+        # TW = J_eMatrix*q_dotMatrix , V = J_vMatrix*q_dotMatrix
+        V = np.matmul(J_vMatrix,q_dotMatrix)
+        X_dot = np.array(V.T)[0]
+        # Return linear velocity as [vx,vy,vz]
+        return X_dot
     
     def cocoax_jacobian_matrix(self, q=[0.0,0.0,0.0]):
         '''
@@ -78,7 +148,7 @@ class CocoaVKinematic(Node):
         # P = Position of Frame[i] via GlobalFrame [3x4]
         # R_e = Rotation matrix of end effector via GlobalFrame [3x3]
         # P_e = Position of end effector via GlobalFrame [3x1]
-        R, P = self.cocoax_forward_kinematic(q)        
+        R, P, R0_e, P0_e = self.cocoax_forward_position_kinematic(q)        
         
         '''
         R = 
@@ -105,12 +175,12 @@ class CocoaVKinematic(Node):
         R0_1 = R[:,:,0] # Frame[1] via GlobalFrame
         R0_2 = R[:,:,1] # Frame[2] via GlobalFrame
         R0_3 = R[:,:,2] # Frame[3] via GlobalFrame
-        R0_e = R[:,:,3] # End Effector via GlobalFrame
+        R0_E = R[:,:,3] # End Effector via GlobalFrame
         
         P0_01 = P[:,0] # Position of Frame[1] via GlobalFrame [3x1]
         P0_02 = P[:,1] # Position of Frame[2] via GlobalFrame [3x1]
         P0_03 = P[:,2] # Position of Frame[3] via GlobalFrame [3x1]
-        P0_E = P[:,3] # Position of End Effector via GlobalFrame [3x1]
+        P0_E  = P[:,3] # Position of End Effector via GlobalFrame [3x1]
 
         # Since all the joints are revolute, the Jacobian p = 1
         rho = [1,1,1]
@@ -143,28 +213,9 @@ class CocoaVKinematic(Node):
         '''
         # Stack the Jacobian matrix of angular velocity and linear velocity to form the Jacobian matrix of End Effector [6x3]
         J_e = np.concatenate((JW,JV),axis=0)
-        
         return J_e
     
-    def cocoax_jacobian_check_singularity(self,J_e):
-        # We only need the last 3 rows of the Jacobian matrix of End Effector it represents the Task Space linear velocity in the Global Frame [X,Y,Z]
-        J_e_star = J_e[3:6,:] # [3x3]
-        # Find the determinant of Reduced Jacobian matrix of End Effector to check singularity
-        # If value of the determinant is undefined(subtrack by zero) the return value is 0.0 So, it always below the threshold
-        J_e_star_det = np.linalg.det(J_e_star)
-        
-        '''
-            return format bool
-        '''
-        # Set threshold of Singularity = 0.001 
-        threshold = 0.001
-        # If the absolute value of determinant of Reduced Jacobian matrix is 0 or below threshold, then the robot is in singularity
-        if np.abs(J_e_star_det) < threshold:
-            return True
-        else:
-            return False
-    
-    def cocoax_forward_kinematic(self, q=[0.0,0.0,0.0]):
+    def cocoax_forward_position_kinematic(self, q=[0.0,0.0,0.0]):
         # Cococax robot parameters
         l1, l2, l3 = self.link_length
         DH = np.matrix([[0, 0, l1, 0, 0],
@@ -210,7 +261,9 @@ class CocoaVKinematic(Node):
         P0_3 = Hlist[2][0:3,3]
         P = np.concatenate((P0_1,P0_2,P0_3,P0_e),axis=1)
         
-        return R, P
+        # Change end effector position to array
+        P0_e = np.array(P0_e.T)[0]
+        return R, P, R0_e, P0_e
         
     def translateX(self,x):
         return np.matrix([[1, 0, 0, x], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
@@ -227,7 +280,7 @@ class CocoaVKinematic(Node):
     def rotateY(self,theta):
         return np.matrix([[np.cos(theta), 0, np.sin(theta), 0], [0, 1, 0, 0], [-np.sin(theta), 0, np.cos(theta), 0], [0, 0, 0, 1]])
     
-    def check_IK_solution_exist(self, x, y, z, r1, r2):
+    def check_IK_position_solution_exist(self, x, y, z, r1, r2):
         [l1,l2,l3] = self.link_length
         A0 = (x**2)+(y**2)+(z**2)+(l1**2)
         A1 = 2*z*l1
@@ -237,17 +290,17 @@ class CocoaVKinematic(Node):
         self.get_logger().info('IK solution does not exist')
         return False
     
-    def check_IK_joint_limit(self, q1, q2, q3):
+    def check_IK_position_joint_limit(self, q1, q2, q3):
         if (-np.pi<= q1 <= np.pi) and (-np.pi/6 <= q2 <= np.pi/6) and (-np.pi/3 <= q3 <= np.pi/2):
             return True
         self.get_logger().info('IK joint limit exceed')
         return False
     
-    def cocoax_inverse_kinematic(self, pos=[0.0,0.0,0.0],r = [1.0,1.0], Lastconfig=[0.0,0.0,0.0]):
+    def cocoax_inverse_position_kinematic(self, X=[0.0,0.0,0.0],r = [1.0,1.0], Lastconfig=[0.0,0.0,0.0]):
         [l1,l2,l3] = self.link_length
-        [x,y,z] = pos
+        [x,y,z] = X
         [r1,r2] = r # r1 = 1 or -1, r2 = 1 or -1
-        if(self.check_IK_solution_exist(x, y, z, r1, r2)):
+        if(self.check_IK_position_solution_exist(x, y, z, r1, r2)):
             # input: [x,y,z] in meter
             # solve for q1
             q1 = np.arctan2(y/r1,x/r1)
@@ -259,7 +312,7 @@ class CocoaVKinematic(Node):
             sin_q2 = ((l3*(z-l1)*np.cos(q3)) - (r1*np.sqrt(x**2+y**2)*(l2+l3*np.sin(q3))))
             cos_q2 = ((-l1*l2)-(l1*l3*np.sin(q3))+(l2*z)+(l3*r1*np.sqrt(x**2+y**2)*np.cos(q3))+(l3*z*np.sin(q3)))
             q2 = np.arctan2(sin_q2,cos_q2)
-            if(self.check_IK_joint_limit(q1, q2, q3)):
+            if(self.check_IK_position_joint_limit(q1, q2, q3)):
                 config = [q1,q2,q3]
                 status = True
                 return config, status
